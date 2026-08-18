29462000a1e57c65b6cfb051dca93dda0fdd6bcb
Yaswanth143612 <yaswanthsrimanthula@gmail.com>
2026-08-18 09:04:30 +0530

    Tokens, identifiers & syntax rules
    learling python Day-2

---
+++ day2_python.py
@@ -0,0 +1,126 @@
+'''
+Tokens
+------
+--> Tokens are the small unit in the python ..
+
+1. Identifier
+-------------
+-->Identifier is a name of varible or function or class
+varible -->
+
+num = 'Python'
+print(type(num))
+
+functions-->
+def add_(a,b):
+    print(a+b)
+
+add_(4,5)
+
+class-->
+class details:
+      pass
+per_1 = details
+
+2. Keywords
+-----------
+--> Keywords are already saved in python for an specified reason to run..
+eg
+--
+if
+else
+for
+while
+return
+print 
+
+
+3. Literals
+-----------
+-->Literals are the datatypes that need to be stored in varibles...
+num = 90
+name = 'Teja'
+
+4. Operator
+-----------
++ , - , = ,
+
+statements
+----------
+-->statements are the instructions given to the program...
+
+num = 90
+age = 2
+if age >= 18:
+    print(age)
+comments
+--------
+--> once comments are open the lines inside will never execute in python file...
+
+1.single line comments (#)
+--------------------------
+--> used to comment only one line
+eg
+--
+age = 20
+if age >=18: #this check age is greater orequal
+print(age)
+
+
+2.multi line comments  (''' ''',""" """ )
+-----------------------------------------
+--> used to comment more than one line
+
+varibles rules
+--------------
+Bad ways
+--------
+--> can't use numbe at 1st position
+--> can't use special char anywhere
+--> can't use space
+--> Keywords
+
+eg
+--
+2num = 90
+$num = 89
+n um = 78
+if = 67
+
+Good ways
+---------
+--> small letters and cap letters , (_) under
+eg
+--
+nUm_1 = 90
+Num_2 = 78
+yashu_sriman = 90
+
+a = {'name':'yashu',
+    'AC_Num' :'7605868391'}
+
+b = {'name':'sriman',
+    'AC_Num' :'7605868392'}
+
+b = {'name':'sri',
+    'AC_Num' :'7605868393'}
+
+IND_yashu_details = {'name':'yashu',
+    'AC_Num' :'7605868391'}
+
+IPPB_sriman_details ={'name':'sriman',
+    'AC_Num' :'7605868392'}
+
+num = 90
+print(num)
+
+
+ 
+'''
+a , b =45,67
+print('a=',a)
+print('b=',b)
+a, b = b, a
+print('a=',a)
+print('b=',b)
+
