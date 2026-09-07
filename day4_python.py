ee6727dc68061c804521eb60b0432ff04d5fb2f7
Yaswanth143612 <yaswanthsrimanthula@gmail.com>
2026-09-07 16:16:59 +0530

    day4
    day 4 of python learning

---
+++ day4_python.py
@@ -0,0 +1,200 @@
+'''
+Concadination
+-------------
+--> The + will behave two ays  of for numeric it work normally and other datatypes list , tuple it concadination.
+
+Operators
+---------
+--> The operators are used perform operations of variables and the values...
+
+1. Arthematic operator
+----------------------
+ like string, 
+--> + , - , * , / , // , %
+
++ --> To add the values 
+num = 44
+num_2 = 21
+print(num + num_2)
+
+- -->sub
+
+a = 9
+b = 5
+print(a - b)
+
+* --> mul
+a = 4
+b = 2
+print(a * b)
+
+/ -> div
+
+a = 4
+b = 2
+print( a / b)
+
+// --> flot div
+
+a = 4
+b = 2
+print( a // b)
+
+eg 2
+
+a = 21
+b = 4
+print( a // b)
+
+% --> mod
+
+a = 21
+b = 4
+print(a % b)
+
+
+
+2. Assignment operator
+----------------------
+  += , -= , *= , %= , /=
+
+ += --> is increment operator
+
+  a = 0
+  print(a)
+  a += 1
+  print(a)
+
+-= --> decrement operator
+
+b = 67
+b -= 41
+print(b)
+
+
+*= -->
+
+b = 67
+b *= 41
+print(b)
+
+/= -->
+
+b= 67
+b /= 41
+print(b)
+
+%= -->
+
+b= 4
+b %= 4
+print(b)
+ 
+
+3. comparision operator
+-----------------------
+== , >= , <= , < , > ,!=
+
+ Num = 9
+ Num_2 = 3
+ print( Num == Num_2)
+ print( Num != Num_2)
+ print( Num >= Num_2)
+ print( Num <= Num_2)
+
+
+ eg
+
+ num = 10
+ num_2 = 9
+ print( num !=num_2)
+ print( num >= num_2)
+ 
+  
+4. logical operator
+--------------------
+ Or
+ Not
+
+
+ num = 9
+ num_2 = 13
+ print(num >= num_2 and num <= 10)
+ print(num <= num_2 and num <= 10)
+
+
+
+5. identity operator
+--------------------
+is
+--
+num = [1,2]
+num_2 = [1,2]
+print(num is num_2)
+
+
+num = 45
+num_2 = 45
+print(num is num_2)
+
+is not
+
+a = [1,2]
+b = [1,2]
+print(id(a))
+print(id(b))
+print(a is b)
+
+
+6. membership operator
+---------------------
+In
+
+nums = [1,2,56,78,423]
+print(78 in nums)
+
+
+nums = 'python'
+print('y' in nums)
+
+
+
+not in
+
+nums = 'python'
+print('k' not in nums)
+
+
+
+7. bitwise operator
+-------------------
+
+
+
+
+
+'''
+
+
+
+Num = 9
+Num_2 = 3
+print( Num == Num_2)
+print( Num != Num_2)
+print( Num >= Num_2)
+print( Num <= Num_2)
+
+num = 9
+num_2 = 13
+print(num >= num_2 and num <= 10)
+print(num <= num_2 and num <= 10)
+
+a = 21
+b = 4
+print( a // b)
+
+
+
+
+
+
